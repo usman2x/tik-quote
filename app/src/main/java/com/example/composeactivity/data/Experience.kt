0beb2be808c67f3c01c42f2558e0113709eff2c3
Muhammad Usman <hafizusmandev@gmail.com>
2025-05-13 15:26:18 +0500

    Removed unnecessary files

--- app/src/main/java/com/example/composeactivity/data/Experience.kt
+++
@@ -1,9 +0,0 @@
-package com.example.composeactivity.data
-
-data class Experience(val title: String, val duration: String)
-
-val experienceList = listOf(
-    Experience("Software Engineer - Company A", "2020 - Present"),
-    Experience("Mobile Developer - Company B", "2018 - 2020"),
-    Experience("Intern - Company C", "2017 - 2018")
-)
